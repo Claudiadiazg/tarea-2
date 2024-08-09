902194393090fb4ed7a995e6a745ad92458fc2bd
Claudia Diaz G <cldiazg@creasistemas.com>
2024-08-09 18:38:10 -0500

    se agrega tarea 2

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Andrés Cepeda</h1>
+      <img src="images.jpg" alt="Andrés Cepeda" />
+      <p>
+        Andrés Cepeda es un reconocido cantautor y músico colombiano nacido el 7 de julio de 1973 en Bogotá.
+        Su estilo musical combina elementos del pop, rock y música latina, y se ha destacado por su voz distintiva
+        y sus letras emotivas. A lo largo de su carrera, Cepeda ha lanzado varios álbumes exitosos y ha ganado numerosos
+        premios, consolidándose como una figura importante en la escena musical latinoamericana.
+
+        Cepeda comenzó su carrera musical en los años 90 como miembro de la banda de rock Poligamia, pero fue su
+        carrera como solista la que realmente lo catapultó al estrellato. Su primer álbum en solitario, “Sé q’ te quiero”,
+        fue lanzado en 2001 y fue bien recibido tanto por críticos como por el público. Desde entonces, ha lanzado
+        varios álbumes que han sido aclamados por su mezcla de estilos musicales y su capacidad para conectar emocionalmente
+        con sus oyentes.
+
+        Además de su carrera musical, Cepeda es conocido por su compromiso con causas sociales y su participación en
+        actividades de beneficencia. Su carácter carismático y su habilidad para comunicarse con su audiencia han hecho
+        de él una figura querida y respetada en la industria musical.
+
+        En resumen, Andrés Cepeda es un artista versátil y apasionado, cuya carrera musical ha sido marcada por su
+        habilidad para fusionar géneros y su capacidad para tocar el corazón de sus seguidores.
+      </p>
+    </div>
+  );
+}
+
+export default App;
